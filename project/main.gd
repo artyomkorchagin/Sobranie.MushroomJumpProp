1ebb677690157db7ae830205653bd034c5765ddd
artyomkorchagin <steamartem@gmail.com>
2024-11-12 19:52:42 +0300

    update 1.0.2
    fixed dependencies, added 4 shrooms

--- project/main.gd
+++ project/main.gd
@@ -5,6 +5,15 @@ const devID = "Sobranie.MushroomJumpProp"
 
 func _ready():
 	Lure.add_actor(devID, "prop_jump_mushroom", "mod://Assets/prop_jump_mushroom.tscn")
-	Lure.add_content(devID, "prop_jump_mushroom", "mod://Resources/prop_jump_mushroom.tres")
+	Lure.add_actor(devID, "prop_jump_mushroom1", "mod://Assets/prop_jump_mushroom.tscn")
+	Lure.add_actor(devID, "prop_jump_mushroom2", "mod://Assets/prop_jump_mushroom.tscn")
+	Lure.add_actor(devID, "prop_jump_mushroom3", "mod://Assets/prop_jump_mushroom.tscn")
+	Lure.add_actor(devID, "prop_jump_mushroom4", "mod://Assets/prop_jump_mushroom.tscn")
+	Lure.add_content(devID, "prop_jump_mushroom", "mod://Resources/prop_jump_mushroom1.tres")
+	Lure.add_content(devID, "prop_jump_mushroom1", "mod://Resources/prop_jump_mushroom2.tres")
+	Lure.add_content(devID, "prop_jump_mushroom2", "mod://Resources/prop_jump_mushroom3.tres")
+	Lure.add_content(devID, "prop_jump_mushroom3", "mod://Resources/prop_jump_mushroom4.tres")
+	Lure.add_content(devID, "prop_jump_mushroom4", "mod://Resources/prop_jump_mushroom5.tres")
+	
 
 
